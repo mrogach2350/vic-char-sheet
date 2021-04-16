cc250bc555c3a73831fc7ef43c32a0f1e5adc63f
Michael Rogachevsky <m.rogach@gmail.com>
2021-04-16 12:23:33 -0700

    WIP

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,79 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="6">
+        <v-card>
+          <v-card-title>Basic Info</v-card-title>
+          <v-card-text>
+            <v-text-field
+              outlined
+              label="Name"
+            ></v-text-field>
+            <v-text-field
+              outlined
+              label="Gender"
+            ></v-text-field>
+            <v-text-field
+              outlined
+              label="Age"
+              type="number"
+              suffix=" years old"
+            ></v-text-field>
+            <v-text-field
+              outlined
+              label="Lineage"
+            ></v-text-field>
+            <v-text-field
+              outlined
+              label="Alignment"
+            ></v-text-field>
+            <v-textarea
+              rows="3"
+              outlined
+              label="Physical Description"
+            ></v-textarea>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="6">
+        <v-card>
+          <v-card-title>Ability Scores</v-card-title>
+          <v-card-text>
+            <v-textarea
+              rows="3"
+              outlined
+              label="Physical Description"
+            ></v-textarea>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="6">
+        <v-card>
+          <v-card-title>Level Up Choices</v-card-title>
+        </v-card>
+        <v-card>
+          <v-card-title>Abilities</v-card-title>
+        </v-card>
+      </v-col>
+      <v-col cols="6">
+        <v-card>
+          <v-card-title>Abilities</v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     HelloWorld
   }
-}
+};
 </script>
