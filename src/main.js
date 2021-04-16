009b6e123819b26edf11292917af1a1a7f36abd4
Michael Rogachevsky <m.rogach@gmail.com>
2021-04-16 16:58:21 -0700

    smooths out firebase connection

--- src/main.js
+++ src/main.js
@@ -5,6 +5,31 @@ import store from './store'
 import vuetify from './plugins/vuetify';
 import '@babel/polyfill'
 
+import firebase from 'firebase';
+import 'firebase/auth';
+import 'firebase/database';
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID
+};
+
+firebase.initializeApp(firebaseConfig)
+firebase.auth().signInAnonymously()
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    console.log('log user in: ', user)
+    firebase.firestore().collection('test').doc('hello').set({ success: true })
+  } else {
+    console.log('log user out')
+  }
+});
+
 Vue.config.productionTip = false
 
 new Vue({
